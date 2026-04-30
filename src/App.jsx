1fd3bd0294b1ac90b6d271521485c5906540dd3c
Your Name <you@example.com>
2026-04-30 14:13:56 +0530

    my first score board

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,70 @@
-import React, { useState } from "react";
+import { useEffect,useState } from 'react'
 import "./App.css";
-function ToDoList() {
 
-  let [tasks, setTasks] = useState([]);
-  let [text, setText] = useState("");
 
-  function change(e){
-    setText(e.target.value);
-  }
+function App() {
+  const [score,setScore] =useState(0)
+  const [ball,setBall] =useState(0)
+  const [over,setOver] =useState(0)
+  const [wicket,setWicket] =useState(0)
 
-  function add(){
-    if(text !== ""){
-      let arr = [...tasks];
-      arr.push(text);
-      setTasks(arr);
-      setText("");
+  const addRun = (run) => {
+    setScore(prev => prev + run);
+    setBall(prev => prev + 1);
+  };
+
+  const addWicket = () => {
+    setWicket(prev => prev + 1);
+    setBall(prev => prev + 1);
+  };
+
+  useEffect(() => {
+    if (ball === 6) {
+      setOver(prev => prev + 1);
+      setBall(0);
     }
-  }
+  }, [ball]);
 
+ 
   return (
-    <div>
+    <div className="body">
+      <div className="box">
+        <h3>CRICKET SCORE BOARD</h3>
 
-      <h1>MY To Do LIST</h1>
+  <div className="row">
+    <span>Score</span>
+     <span>{score}</span>
+  </div>
 
-      <input value={text} onChange={change} />
-      <button onClick={add}>Add</button>
+  <div className="row">
+    <span>Ball</span>
+    <span>{ball}</span>
+  </div>
 
-      <ul>
-        {tasks.map((t, i) => (
-          <li key={i}>{t}</li>
-        ))}
-      </ul>
+  <div className="row">
+    <span>Overs</span>
+   <span>{over}</span>
+  </div>
 
+  <div className="row">
+    <span>Wickets</span>
+    <span>{wicket}</span>
+  </div>
+
+        <div className="buttons">
+          <button onClick={() => addRun(1)}>1 Run</button>
+          <button onClick={() => addRun(2)}>2 Run</button>
+          <button onClick={() => addRun(3)}>3 Run</button>
+          <button onClick={() => addRun(4)}>4 Run</button>
+          <button onClick={() => addRun(6)}>6 Run</button>
+        </div>
+
+        <button className="wicket-button" onClick={addWicket}>
+          Wicket
+        </button>
+      </div>
     </div>
   );
 }
 
-export default ToDoList;
+export default App;
